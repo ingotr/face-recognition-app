52468d14d75ec90ae8f6d1d8def2e87050bb6481
Aleksandr <markovkin88@gmail.com>
2020-06-16 22:50:33 +0300

    15: Linked Api to input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
@@ -29,22 +30,24 @@ class App extends React.Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log("test");
+    this.setState({imageUrl: this.state.input})
     app.models
       .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
-        "https://samples.clarifai.com/face-det.jpg")
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
       .then(
         function (response) {
-          console.log(response);
+          console.log(response
+            .outputs[0].data.regions[0].region_info.bounding_box);
         },
         function (err) {
           // there was an error
@@ -65,9 +68,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
-
-        <FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
