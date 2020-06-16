3e69c3c713d33ae1db2c26cfc0a8701aba039b25
Aleksandr <markovkin88@gmail.com>
2020-06-16 20:55:19 +0300

    4: Added navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
+import 'tachyons';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
     </div>
   );
 }
