5ce5db5cb7b70cb61dcea678b18a4a3d269ed782
Aleksandr <markovkin88@gmail.com>
2020-06-16 21:46:49 +0300

    12: Updated app background with particles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 import 'tachyons';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={particlesOptions}
+        className='particles'/>
       <Navigation />
       <Logo />
       <Rank />
